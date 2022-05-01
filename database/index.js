dae658e1033119ac93d66fce863474480fb4560b
Gaurang Shukla <gaurangshukla99@gmail.com>
2022-05-02 00:50:02 +0530

    database

---
+++ database/index.js
@@ -0,0 +1,9 @@
+module.exports.init = function(){
+    const mongoose = require('mongoose');
+    mongoose.connect('mongodb+srv://<username>:<password>@cluster0.rq2ix.mongodb.net/ecommone?retryWrites=true&w=majority')
+    .then(function(){
+      console.log("db is on");
+    }).catch(function(){
+      console.log("db is off");
+    })
+  }
