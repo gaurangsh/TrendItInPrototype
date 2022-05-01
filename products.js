c0aec25bcb9177c675f99023689b73046236032a
Gaurang Shukla <gaurangshukla99@gmail.com>
2022-05-02 00:55:25 +0530

    products.js

---
+++ products.js
@@ -0,0 +1,26 @@
+[
+    {
+      "_id":1,
+      "name":"TV",
+      "price":"Rs. 75000",
+      "description":"Its 55 inch, AMOLED Panel"
+    },
+    {
+      "_id":2,
+      "name":"iPhone",
+      "price":"Rs. 69000",
+      "description":"Its 6.9 inch, M1 chipset"
+    },
+    {
+      "_id":3,
+      "name":"Car",
+      "price":"Rs. 1500000",
+      "description":"iVtec, Petrol Engine"
+    },
+    {
+      "_id":4,
+      "name":"Bike",
+      "price":"Rs. 80000",
+      "description":"Triumph, Street triple"
+    }
+  ]
